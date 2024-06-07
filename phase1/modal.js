a77fed731ee43da3d0e1493b504de5a0fc2a4e2d
qmarkm <106965891+qmarkm@users.noreply.github.com>
2024-06-07 17:23:35 +0800

    Finalized post_page.html
    Added edit and delete buttons on comments, moved css of post_page.html from phase1/css/style.css to phase1/style.css

--- phase1/modal.js
+++ phase1/modal.js
@@ -2,3 +2,24 @@ function togglePopup()
 {
     document.getElementById("popup_js").classList.toggle("active");
 }
+
+function toggleReplyComment(button) {
+    button.parentElement.getElementsByTagName("div")[0].style.display = (button.parentElement.getElementsByTagName("div")[0].style.display === 'block') ? 'none' : 'block';
+}
+
+function editCommentDiv(button){
+    comment = button.parentElement.parentElement.getElementsByTagName("p")[0];
+    actionButtons = button.parentElement.parentElement.getElementsByTagName("div")[0];
+
+    if(comment.classList.contains("draftReplyComment")){
+        comment.classList.remove("draftReplyComment");
+        comment.setAttribute('contenteditable','false');
+
+        actionButtons.classList.add('editCommentButtons');
+    } else {
+        comment.setAttribute('contenteditable','true');
+        comment.classList.add('draftReplyComment');
+
+        actionButtons.classList.remove('editCommentButtons');
+    }
+}
